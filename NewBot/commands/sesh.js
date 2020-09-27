cb44176a348eeab4d2bc84ae430e7f9c731d4374
SimonCobb <simoncobb@msn.com>
2020-09-27 18:05:18 +0100

    Add another dancer

--- NewBot/commands/sesh.js
+++ NewBot/commands/sesh.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'Start the party',
 	cooldown: 5,
 	execute(message) {
-		message.channel.send(':tada: :beers: :dancer:');
+		message.channel.send(':tada: :beers: :dancer: :male_dancer:');
 	},
 };
